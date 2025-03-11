f6456565dec46a8096d56abd013dec49daf55f56
Anna Clara <anisferraz@gmail.com>
2025-03-11 20:21:27 -0300

    feat: add infinite scrolling component for dynamic content loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import { Process } from "@/components/process";
 import { Services } from "@/components/services";
 import { Founders } from "@/components/founders";
 import { AboutUs } from "@/components/about-us";
+import { InfiniteScrolling } from "@/components/infinite-scrolling";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <InfiniteScrolling />
       <AboutUs />
       <Cases />
       <Process />
---
+++ src/components/infinite-scrolling.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from 'react';
+
+import { motion } from "framer-motion";
+
+const words: String[] = ["Tráfego Pago", "Shopify Experts", "Influenciadores"];
+
+export function InfiniteScrolling() {
+  return (
+    <section className="py-5 md:py-5">
+      <div className="container">
+        <div className="max-w-[1020px] mx-auto flex relative overflow-hidden [mask-image:linear-gradient(to_right,#00000000_0%,#000_12%,#000_87%,#00000000_100%)]">
+          <motion.ul
+            transition={{
+              duration: 20,
+              ease: 'linear',
+              repeat: Infinity,
+            }}
+            initial={{ translateX: 0 }}
+            animate={{ translateX: '-50%' }}
+            className="flex flex-none gap-8 md:gap-16 pr-16"
+          >
+            {[...new Array(6)].fill(0).map((_, i) => (
+              <React.Fragment key={i}>
+                {words.map((word, index) => (
+                  <li  key={`${i}-${index}`} className="text-base md:text-lg text-muted">
+                    {word}
+                  </li>
+                ))}
+              </React.Fragment>
+            ))}
+          </motion.ul>
+        </div>
+      </div>
+    </section>
+  )
+};
