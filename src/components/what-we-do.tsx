179d36dea787fb139a83489d7cdff28330c6a49d
Anna Clara <anisferraz@gmail.com>
2025-03-13 22:08:28 -0300

    feat: adjust responsiveness

--- src/components/what-we-do.tsx
+++ src/components/what-we-do.tsx
@@ -29,7 +29,7 @@ export function WhatWeDo() {
           </RevealText>
         </div>
 
-        <div className="grid grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-10">
           <AnimatedSVG />
           
           <div className="max-w-4xl mx-auto">
